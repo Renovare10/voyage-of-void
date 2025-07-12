1a71b50fcf1f5de955dfafc9fe3235d8108d27a0
Chadfire <chadfire10@yahoo.com>
2025-07-11 20:15:07 -0400

    Change world to blue

--- Scripts/Entities/Player/player.gd
+++ Scripts/Entities/Player/player.gd
@@ -1,24 +1,32 @@
 extends CharacterBody2D
 
-
 @export var speed: float = 300.0
-var target_position = position
+@export var movement_threshold: float = 5.0
+
+@export var raft_walkable_area: Rect2 = Rect2(-100, -50, 200, 100)
+
+var target_local_position: Vector2
 
 func _ready() -> void:
-	target_position = global_position
+	target_local_position = position
 
 func _physics_process(_delta: float) -> void:
-	# Calculate the direction vector from the current position to the target position
-	var direction: Vector2 = target_position - global_position
+	var direction: Vector2 = target_local_position - position
 
-	if direction.length() > 5:
+	if direction.length() > movement_threshold:
 		velocity = direction.normalized() * speed
 	else:
 		velocity = Vector2.ZERO
-		
-	# Move character
+
 	move_and_slide()
-	
+
+	position.x = clamp(position.x, raft_walkable_area.position.x, raft_walkable_area.position.x + raft_walkable_area.size.x)
+	position.y = clamp(position.y, raft_walkable_area.position.y, raft_walkable_area.position.y + raft_walkable_area.size.y)
+
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
-		target_position = get_global_mouse_position()
+		if get_parent():
+			target_local_position = get_parent().to_local(get_global_mouse_position())
+		else:
+			target_local_position = get_global_mouse_position()
