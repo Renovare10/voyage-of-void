5623ed36eacb5133a3c3384ced674e00c80b690b
Chadfire <chadfire10@yahoo.com>
2025-07-14 10:09:05 -0400

    Rename port

---
+++ Scripts/CityData.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name CityData
+
+@export var name: String
+@export var position: Vector2
---
+++ Scripts/Global.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@export var cities: Array[CityData] = []  # Populate in Inspector: add CityData instances
+var current_city_name: String = "StartingIsland"
+
+func get_city_position(city_name: String) -> Vector2:
+	for city in cities:
+		if city.name == city_name:
+			return city.position
+	return Vector2.ZERO
+
+func set_player_city(new_city_name: String):
+	if get_city_position(new_city_name) != Vector2.ZERO:
+		current_city_name = new_city_name
