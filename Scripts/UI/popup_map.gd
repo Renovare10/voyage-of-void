21d4f198f98aa7e37d37e4fb69d5f9aae51cc8a6
Chadfire <chadfire10@yahoo.com>
2025-07-16 09:22:42 -0400

    Added Saltwick map

--- Scripts/UI/popup_map.gd
+++ Scripts/UI/popup_map.gd
@@ -2,10 +2,15 @@ extends Control
 
 @onready var map_ui_element = self
 
+@export var location_container_path: NodePath
+@export var next_location_scene: PackedScene
+
+var location_container: Node
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	map_ui_element.visible = false
-
+	location_container = get_node(location_container_path)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -17,4 +22,14 @@ func _unhandled_input(event):
 
 
 func _on_btn_saltwick_pressed() -> void:
-	pass # Replace with function body.
+	# Unload the current location if one exists
+	if location_container.get_child_count() > 0:
+		var current_location = location_container.get_child(0)
+		current_location.queue_free()
+	
+	# Load and add the new location
+	var new_location = next_location_scene.instantiate()
+	location_container.add_child(new_location)
+	
+	# Optional: Hide the map UI after jump
+	self.visible = false
